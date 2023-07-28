5a8b7092c7dab3f7873880e9f779d5434a8d213e
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-28 08:45:50 -0500

    pagina encontrada

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,16 +1,25 @@
-const http = require('node:http')
+import { createServer } from 'node:http'
 
 const PORT = process.env.PORT ?? 4000
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html ; charset=utf-8')
+
   if (req.url === '/') {
-    res.statusCode = 200 // ** OK **
-    res.setHeader('Content-Type', 'text/plain ; charset=utf-8')
+    //* Codigo de Estado
+    res.statusCode = 200 //* OK */
+    //* Enviar Cabecera
     res.end('Bienvenido a mi página de inicio')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // **OK
+    res.end('Contacto')
+  } else {
+    res.statusCode = 404 // **NOT FOUND **
+    res.end('<h1>404</h1> <p>pagina no encontrada </p>')
   }
 }
 
-const server = http.createServer(processRequest)
+const server = createServer(processRequest)
 
 server.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`)
