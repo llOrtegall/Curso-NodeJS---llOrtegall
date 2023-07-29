6c43a50a96f7adc6b8d49ab8b8ae9efbb0ca2878
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-29 11:04:48 -0500

    agregando el metodo 404

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,11 +1,14 @@
 const express = require('express')
-
-const app = express()
+const ditto = require('./pokemon/ditto.json')
 
 const PORT = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome' })
+const app = express()
+//* es buena pradctica quitar esta cabecera por seguridad
+app.disable('x-powered-by')
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
@@ -19,6 +22,10 @@ app.post('/pokemon', (req, res) => {
     data.timestamp = Date.now()
     res.status(201).json(data)
   })
+})
+
+app.use((req, res) => {
+  res.status(404).send('<h1>Not Found: 404</h1>')
 })
 
 app.listen(PORT, () => {
