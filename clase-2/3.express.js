9884eeb71963dab374ed35a82f58574598152665
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-29 11:17:40 -0500

    agregando mi primer middleware

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -7,6 +7,28 @@ const app = express()
 //* es buena pradctica quitar esta cabecera por seguridad
 app.disable('x-powered-by')
 
+app.use((req, res, next) => {
+  // TODO: trackear la request a la base de datos
+  // TODO: revisar si el usuario tiene cookies
+  if (req.method !== 'POST') return next()
+  if (req.headers['content-type'] !== 'application/json') return next()
+
+  let body = ''
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    // mutar la request y meter la información en la req.body
+    req.body = data
+    next()
+  })
+
+  next()
+})
+
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
@@ -24,6 +46,7 @@ app.post('/pokemon', (req, res) => {
   })
 })
 
+// TODO: en lo posible que sea la ultima linea
 app.use((req, res) => {
   res.status(404).send('<h1>Not Found: 404</h1>')
 })
