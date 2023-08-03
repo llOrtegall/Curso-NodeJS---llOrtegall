586ebe24ea4385691d06eccfb536a82b12afaf4d
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-03 11:47:14 -0500

    vamos bien con standard

--- clase-3/app.js
+++ clase-3/app.js
@@ -9,6 +9,10 @@ app.get('/movies', (req, res) => {
   res.json(movies)
 })
 
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+})
+
 const PORT = process.env.PORT || 1234
 
 app.listen(PORT, () => {
