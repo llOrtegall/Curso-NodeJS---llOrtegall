2d05507cb4c5525a0d3fcdb4776160457c281579
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-05 10:54:03 -0500

    recuperar todas las peliculas

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,11 +1,12 @@
-import express from 'express'
+const express = require('express')
+const movies = require('./movies.json')
 
 const app = express()
 
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world' })
+app.get('/movies', (req, res) => {
+  res.json(movies)
 })
 
 const PORT = process.env.PORT || 1234
