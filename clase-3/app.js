c9c742940457e8c31b4fa4f55cd687da7c069ba2
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-05 10:58:28 -0500

    peticion por id creada

--- clase-3/app.js
+++ clase-3/app.js
@@ -9,6 +9,18 @@ app.get('/movies', (req, res) => {
   res.json(movies)
 })
 
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(m => m.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({
+    message: 'Movie not found'
+  })
+})
+
+
+
 const PORT = process.env.PORT || 1234
 
 app.listen(PORT, () => {
