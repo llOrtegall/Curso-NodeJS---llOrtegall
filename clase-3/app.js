b1452b1a68bf385bed985bab85f6710c8490081a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-05 10:47:18 -0500

    agregando dependencias y iniciando

---
+++ clase-3/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, world' })
+})
+
+const PORT = process.env.PORT || 1234
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
