044d4532034d88ab895a5bfd58276d1e69c0feb9
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-07 09:04:11 -0500

    commonJS

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function sum(a, b) {
-  return a + b;
-}
+//CommonJS require module
+const { sum } = require('./sum.js')
 
 console.log(sum(1, 2));
---
+++ sum.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  return a + b;
+}
+
+//Common JS forma de exportar 
+module.exports = { sum }
