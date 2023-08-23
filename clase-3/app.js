35c7ee7ef3e8b2afd1288d076dbe0e687e52cd53
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-23 11:33:02 -0500

    agregando los origenes en la api

--- clase-3/app.js
+++ clase-3/app.js
@@ -7,7 +7,19 @@ const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
 
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://movies.com',
+  'http://midu.dev'
+]
+
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  }
+
   const { genre } = req.query
   if (genre) {
     const filterMovies = movies.filter(
