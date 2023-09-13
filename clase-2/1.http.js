d682dacc456b881a569398467ca1ec053f4755ff
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-12 23:14:51 -0500

    ejemplos de servidor con rutas

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -4,10 +4,18 @@ const PORT = process.env.PORT ?? 3200
 
 const processRequest = (req, res) => {
   // console.log('request received', req.url)
+
+  res.setHeader('Content-Type', 'text/html; charset=utf8')
+
   if (req.url === '/') {
     res.statusCode = 200 // ok
-    res.setHeader('Content-Type', 'text/plain; charset=utf8')
-    res.end('Bienvenido a mi página')
+    res.end('<h3>Bienvenido a mi página De Inicio</h3>')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // ok
+    res.end('<h3>Contactos</h3>')
+  } else {
+    res.statusCode = 404 //
+    res.end('<h3>Error 404 Página No Encontrada</h3>')
   }
 }
 
