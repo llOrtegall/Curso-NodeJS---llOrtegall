b98939ce3ceb0694f30032f8f161df1e88ccdf75
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-21 21:50:03 -0500

    arreglando error cors

--- clase-3/Schemas/movies.js
+++ clase-3/Schemas/movies.js
@@ -5,12 +5,16 @@ const movieSchema = z.object({
   year: z.number().int().min(1900).max(2025),
   director: z.string(),
   duration: z.number().int().positive(),
-  rate: z.number().min(0).max(10),
+  rate: z.number().min(0).max(10).default(5.5),
   poster: z.string().url(),
   genre: z.array(z.string(
-    z.enum(['Action', 'Drama', 'Adventure', 'Comedy', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi'])
+    z.enum(['Action', 'Drama', 'Adventure', 'Crime', 'Comedy', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi'])
   ))
 })
+
+export function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
 
 export function validateMovie (object) {
   return movieSchema.safeParse(object)
--- clase-3/app.js
+++ clase-3/app.js
@@ -1,18 +1,30 @@
 import express from 'express'
 import movies from "./movies.json" assert { type: "json" }
 import crypto from "crypto";
-import { validateMovie } from './Schemas/movies.js';
+import { validateMovie, validatePartialMovie } from './Schemas/movies.js';
 
 const app = express()
 
 app.disable('x-powered-by')
 app.use(express.json())
 
+const ACCEPTED_ORIGINS = [
+  'http://localhost:3030',
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://movies.com'
+]
+
+// TODO: test 
 app.get('/', (req, res) => {
   res.json({ message: 'Hola Mundo' })
 })
-
+// TODO: Recupera todas las peliculas
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  }
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -22,7 +34,6 @@ app.get('/movies', (req, res) => {
   }
   res.json(movies)
 })
-
 // TODO: Recuperar pelicula por ID
 app.get('/movies/:id', (req, res) => {
   const { id } = req.params
@@ -30,28 +41,44 @@ app.get('/movies/:id', (req, res) => {
   if (movie) return res.json(movie)
   res.status(404).json({ message: 'Movie not found' })
 })
-
+// TODO: Crear pelicula y validar datos recibidos
 app.post('/movies', (req, res) => {
   const result = validateMovie(req.body)
 
-  /* const { title, genre, year, director, duration, rate, poster } = req.body
-  TODO: Si algo llega sin datos
-  if (!title || !genre || !year || !director || !duration || !rate || !poster) {
-  return res.status(400).json({ message: 'Missing required fields' })
-  */
-
-  if(result.error){
-    return res.status(400).json({error: JSON.parse(result.error.message)})
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
-
   const newMovie = {
     id: crypto.randomUUID(),
-    // title, genre, year, director, duration, rate: rate ?? 0, poster
     ...result.data
   }
+
   movies.push(newMovie)
 
   res.status(201).json({ message: 'Movie Created', source: newMovie })
+})
+// TODO: Actuliazando las pelicula y validar datos recibidos
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(m => m.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie no found" })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  return res.json(updateMovie)
+
 })
 
 const PORT = process.env.PORT ?? 3030
