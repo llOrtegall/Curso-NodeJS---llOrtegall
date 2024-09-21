71ff2a7d5761212e1c159653f5b9ced865473b49
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-21 14:02:55 -0500

    Update WebSocket client UI and dependencies

--- WebSocket/src/index.ts
+++ WebSocket/src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 import morgan from 'morgan';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 const port = 3000;
 
 app.use(morgan('dev'));
@@ -10,7 +14,15 @@ app.get('/', (req: Request, res: Response) => {
   res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+})
