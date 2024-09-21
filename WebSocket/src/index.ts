a6767ff51c768067e9ec6e5393b903a6d1d81f6e
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-21 13:43:32 -0500

    Refactor project structure and dependencies

--- WebSocket/src/index.ts
+++ WebSocket/src/index.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response } from 'express';
-import { WebSocketServer, WebSocket } from 'ws';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
 
+app.use(morgan('dev'));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+  res.send('<h1>Hello World!</h1>');
 });
 
-const Server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-const wss = new WebSocketServer({ server: Server });
-
-wss.on('connection', (ws: WebSocket) => {
-  console.log(ws);
-});
